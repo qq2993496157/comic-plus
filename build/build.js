2c1d86b1e11e03e16feff80d2c63994a27af09c4
stop <2993496157@qq.com>
2024-09-12 16:38:06 +0800

    1

--- build/build.js
+++ build/build.js
@@ -1,31 +1,42 @@
 import cpy from 'cpy';
 import { resolve } from 'path';
 import fs from 'fs';
+import CleanCSS from 'clean-css';
 
-//组件
+// 组件
 const sourceDir = resolve(__dirname, '../packages/components');
 const targetLib = resolve(__dirname, '../lib/components');
 const targetEs = resolve(__dirname, '../es/components');
 
-//icon
+// icon
 const iconDir = resolve(__dirname, '../packages/icons');
 const targetLibIcon = resolve(__dirname, '../lib/icons');
 const targetEsIcon = resolve(__dirname, '../es/icons');
 
-//typings
+// typings
 const sourceDirRoot = resolve(__dirname, '../packages/typings');
 const targetRoot = resolve(__dirname, '../typings');
 
+const minifyCssFiles = async (sourceDir, targetDir) => {
+  const cssFiles = await cpy(`${sourceDir}/**/*.css`, targetDir, { onlyFiles: true });
+  for (const file of cssFiles) {
+    const filePath = resolve(targetDir, file.replace(`${sourceDir}/`, ''));
+    const cssContent = fs.readFileSync(filePath, 'utf8');
+    const minifiedCss = new CleanCSS().minify(cssContent).styles; //在这里压缩css
+    fs.writeFileSync(filePath, minifiedCss, 'utf8');
+  }
+};
+
 const buildCss = async () => {
-  //copy css文件
-  await cpy(`${sourceDir}/**/*.css`, targetLib);
-  await cpy(`${sourceDir}/**/*.css`, targetEs);
-  await cpy(`${iconDir}/**/*.css`, targetLibIcon);
-  await cpy(`${iconDir}/**/*.css`, targetEsIcon);
-  //copy type文件
+  await minifyCssFiles(sourceDir, targetLib);
+  await minifyCssFiles(sourceDir, targetEs);
+  await minifyCssFiles(iconDir, targetLibIcon);
+  await minifyCssFiles(iconDir, targetEsIcon);
+
+  // copy type files
   await cpy(`${sourceDirRoot}/*.ts`, targetRoot);
 
-  //copy文件之后 由于文件路径发生变化 需要将global.d.ts文件中的相对路径替换掉
+  // update global.d.ts
   const globalDtsPath = resolve(targetRoot, 'global.d.ts');
   try {
     let fileContent = fs.readFileSync(globalDtsPath, 'utf8');
@@ -36,4 +47,5 @@ const buildCss = async () => {
     console.error('Error processing global.d.ts:', err);
   }
 };
+
 buildCss();
