5a198a1391d411547a8c784ce0c21780eab7d5d2
stop <2993496157@qq.com>
2024-09-12 14:28:52 +0800

    1

--- build/build.js
+++ build/build.js
@@ -1,5 +1,7 @@
 import cpy from 'cpy';
 import { resolve } from 'path';
+import fs from 'fs';
+
 //组件
 const sourceDir = resolve(__dirname, '../packages/components');
 const targetLib = resolve(__dirname, '../lib/components');
@@ -22,5 +24,16 @@ const buildCss = async () => {
   await cpy(`${iconDir}/**/*.css`, targetEsIcon);
   //copy type文件
   await cpy(`${sourceDirRoot}/*.ts`, targetRoot);
+
+  //copy文件之后 由于文件路径发生变化 需要将global.d.ts文件中的相对路径替换掉
+  const globalDtsPath = resolve(targetRoot, 'global.d.ts');
+  try {
+    let fileContent = fs.readFileSync(globalDtsPath, 'utf8');
+    fileContent = fileContent.replace(/import\(['"]([^'"]+)['"]\)/g, "import('comic-plus')");
+    fs.writeFileSync(globalDtsPath, fileContent, 'utf8');
+    console.log('Successfully replaced imports in global.d.ts');
+  } catch (err) {
+    console.error('Error processing global.d.ts:', err);
+  }
 };
 buildCss();
