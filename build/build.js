098a20c2b6d50746af6c3e6f8c21544934a719e6
stop <2993496157@qq.com>
2024-09-12 14:13:33 +0800

    1

--- build/build.js
+++ build/build.js
@@ -1,22 +1,26 @@
 import cpy from 'cpy';
 import { resolve } from 'path';
+//组件
 const sourceDir = resolve(__dirname, '../packages/components');
 const targetLib = resolve(__dirname, '../lib/components');
 const targetEs = resolve(__dirname, '../es/components');
+
+//icon
 const iconDir = resolve(__dirname, '../packages/icons');
 const targetLibIcon = resolve(__dirname, '../lib/icons');
 const targetEsIcon = resolve(__dirname, '../es/icons');
 
-const sourceDirRoot = resolve(__dirname, '../packages');
-const targetLibRoot = resolve(__dirname, '../lib/typings');
-const targetEsRoot = resolve(__dirname, '../es/typings');
+//typings
+const sourceDirRoot = resolve(__dirname, '../packages/typings');
+const targetRoot = resolve(__dirname, '../typings');
 
 const buildCss = async () => {
+  //copy css文件
   await cpy(`${sourceDir}/**/*.css`, targetLib);
   await cpy(`${sourceDir}/**/*.css`, targetEs);
   await cpy(`${iconDir}/**/*.css`, targetLibIcon);
   await cpy(`${iconDir}/**/*.css`, targetEsIcon);
-  await cpy(`${sourceDirRoot}/typings/*.d.ts`, targetLibRoot);
-  await cpy(`${sourceDirRoot}/typings/*.d.ts`, targetEsRoot);
+  //copy type文件
+  await cpy(`${sourceDirRoot}/*.ts`, targetRoot);
 };
 buildCss();
